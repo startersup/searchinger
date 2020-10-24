ba30445a9a3dbc694653113c642630d64419237e
Saicharan <saicharan14996@gmail.com>
2020-10-24 16:09:26 +0530

    custom js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -152,7 +152,7 @@ $(document).on('keyup', '.loadDropdown_input', function () {
   var strSearch = $(this).val().toLowerCase();
   var temp = loadDropDownDiv(dropDownType, strSearch);
 
-  $('.loadDropdown_div_ul').html(temp);
+  $('.loadDropdown_div').html(temp);
 
 });
 
