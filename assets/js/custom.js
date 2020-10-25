2f315dae479aa5aa882deb0e373db5d7fbc3bae6
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-10-25 17:05:56 +0530

    dropdown

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,40 +4,40 @@ var mySite = window.location.host;
 var myUrl = myProtocol + "//" + mySite + "/";
 var dropDownList = {
   "businessCategory": [
-    {"id":1,"name":"All Categories"},
-     {"id":2,"name":"Arts"},
-     {"id":3,"name":"Business"},
-     {"id":4,"name":"Computer"},
-     {"id":5,"name":"Digital Marketing"},
-     {"id":6,"name":"Education"},
-     {"id":7,"name":"Finance"},
-     {"id":8,"name":"Food"},
-     {"id":9,"name":"Games"},
-     {"id":10,"name":"Health"},
-     {"id":11,"name":"Home"},
-     {"id":12,"name":"HR"},
-     {"id":13,"name":"Jewellers"},
-     {"id":14,"name":"Junk Removal"},
-     {"id":15,"name":"Kids and Teens"},
-     {"id":16,"name":"Lifestyle"},
-     {"id":17,"name":"Money and Finance"},
-     {"id":18,"name":"News"},
-     {"id":19,"name":"Pest Control"},
-     {"id":20,"name":"Photo Studio"},
-     {"id":21,"name":"Photography"},
-     {"id":22,"name":"Recreation"},
-     {"id":23,"name":"Reference"},
-     {"id":24,"name":"Regional"},
-     {"id":25,"name":"SAAS"},
-     {"id":26,"name":"Science"},
-     {"id":27,"name":"SEO Services"},
-     {"id":28,"name":"Shopping"},
-     {"id":29,"name":"Society"},
-     {"id":30,"name":"Sports"},
-     {"id":31,"name":"Transport"},
-     {"id":32,"name":"Travel"},
-     {"id":33,"name":"Waste Clearance"},
-     {"id":34,"name":"Web Directory"}
+    { "id": 1, "name": "All Categories" },
+    { "id": 2, "name": "Arts" },
+    { "id": 3, "name": "Business" },
+    { "id": 4, "name": "Computer" },
+    { "id": 5, "name": "Digital Marketing" },
+    { "id": 6, "name": "Education" },
+    { "id": 7, "name": "Finance" },
+    { "id": 8, "name": "Food" },
+    { "id": 9, "name": "Games" },
+    { "id": 10, "name": "Health" },
+    { "id": 11, "name": "Home" },
+    { "id": 12, "name": "HR" },
+    { "id": 13, "name": "Jewellers" },
+    { "id": 14, "name": "Junk Removal" },
+    { "id": 15, "name": "Kids and Teens" },
+    { "id": 16, "name": "Lifestyle" },
+    { "id": 17, "name": "Money and Finance" },
+    { "id": 18, "name": "News" },
+    { "id": 19, "name": "Pest Control" },
+    { "id": 20, "name": "Photo Studio" },
+    { "id": 21, "name": "Photography" },
+    { "id": 22, "name": "Recreation" },
+    { "id": 23, "name": "Reference" },
+    { "id": 24, "name": "Regional" },
+    { "id": 25, "name": "SAAS" },
+    { "id": 26, "name": "Science" },
+    { "id": 27, "name": "SEO Services" },
+    { "id": 28, "name": "Shopping" },
+    { "id": 29, "name": "Society" },
+    { "id": 30, "name": "Sports" },
+    { "id": 31, "name": "Transport" },
+    { "id": 32, "name": "Travel" },
+    { "id": 33, "name": "Waste Clearance" },
+    { "id": 34, "name": "Web Directory" }
 
   ]
 };
@@ -125,9 +125,10 @@ $(document).on('focus', '.loadDropdown', function () {
 
   var temp = '<div class="loadDropdown_div" style=" box-shadow: 0 3px 20px rgba(0,0,0,.2);border-radius: .25rem;padding: 15px;position: absolute;width: 350px;z-index: 100;background-color: #ffffff;height:300px;" ><input type="text" value="" drop-type="' + dropDownType + '" class="loadDropdown_input" style="border: 1px solid rgba(252,68,18,.3) !important;width:100%;margin-bottom:10px;" ><br>';
   var temp2 = loadDropDownDiv(dropDownType, 'a');
-  temp = temp +'<ul class="loadDropdown_ul" style="list-style-type: none;margin:0;padding:0;height:250px;overflow-y:scroll;">'+ temp2 + '</ul></div>';
+  temp = temp + '<ul class="loadDropdown_ul" style="list-style-type: none;margin:0;padding:0;height:250px;overflow-y:scroll;">' + temp2 + '</ul></div>';
   $('.loadDropdown_div').remove();
   $(this).parent().append(temp);
+  return false;
 });
 
 function loadDropDownDiv(dropDownType, strSearch) {
@@ -157,7 +158,6 @@ $(document).on('keyup', '.loadDropdown_input', function () {
 });
 
 $(document).on('click', '.loadDropdown_class_li', function () {
-
   var dropDownType = $('.loadDropdown_input').attr("drop-type");
   var temp = '';
   temp = temp + '<option value="' + $(this).attr("select-val") + '" >' + $(this).html() + '</option>';
@@ -168,6 +168,6 @@ $(document).on('click', '.loadDropdown_class_li', function () {
     }
 
   });
- 
+
   $('.loadDropdown_div').remove();
 });
