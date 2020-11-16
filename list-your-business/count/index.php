7e95e81b139667d39ae2491e3ee506e033f605e0
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-11-16 19:12:46 +0530

    count

--- list-your-business/count/index.php
+++ list-your-business/count/index.php
@@ -9,8 +9,13 @@ $result =  mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $totalBusinessList = $row["totalCount"];
 
-echo($conn);
-echo("<br>");
+echo($result);
+if($result)
+{
+    echo("<br> success result");
+}
+
+
 // echo("<h3>Total Business Enrolled => $totalBusinessList </h3>");
 
 $sql="SELECT count(id) as totalCount from imageServer ";
