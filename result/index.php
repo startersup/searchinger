ba2d243028cb64f4d42a0ab2e6976dd0ec2e4eb9
Saicharan <saicharan14996@gmail.com>
2021-06-06 12:27:44 +0530

    nav dynamic
    nav dynamic

--- result/index.php
+++ result/index.php
@@ -39,34 +39,11 @@ $rootfolder = $_SERVER['DOCUMENT_ROOT'];
 </head>
 
 <body style="background:#f2f3f7;">
-  <nav class="sng-navigation navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="../index.html"><span>Search</span>inger</a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li class="dropdown sng-dropdown">
-            <a class="dropdown-toggle custom-arrow" href="#">Categories</a>
-            <div class="sng-dropdown-content">
+<?php
 
-            </div>
-          </li>
+include($rootfolder.'/navBar.php');
 
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="./list-your-business/" class="sng-free-business">+ List your Business</a></li>
-          <li><a href="#" class="sng-post-ad">Post Free Ad</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+ ?>
   <section class="sng-results">
     <div class="container">
       <div class="col-md-12">
